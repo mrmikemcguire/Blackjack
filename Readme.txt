fa241175d81740f4959d0ba18557ecec0f328af4
mcguire <mcguire@mullenhigh.com>
2015-10-23 10:17:32 -0600

    Prior to major rewrite

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,1 @@
 This is my attempt to apply the CardMechanics skeleton to a specific game.
-Having VCS issues, though...:(
--- blackjack.js
+++ blackjack.js
@@ -1,89 +1,93 @@
 shuffle(deck);
 $('#sound')[0].play();
+var z = 1;
+var discardLocation;
 
-function cardAlert (clickEvent) 
-	{
-  	var $card, data;
-  	$card = $(clickEvent.target);
-  	data = $card.data('card');
+function cardAlert (clickEvent)
+{
+    var $card, data;
+    $card = $(clickEvent.target);
+    data = $card.data('card');
 //  	alert(data.rank + " of " + data.suit);
     $card.addClass('played');
     $card.removeClass('in-d-hand');
     $card.removeClass('in-p-hand');
     $card.css('left', discardLocation);
-	}
+    $card.css('z-index', z);
+    z ++;
+}
 
-$('#deal').on("click", function()
+$('#deal').on('click',function()
+{
+    numberOfCards = $('#numberOfCards').val();
+    discardLocation = (((numberOfCards * 12) + 180) / 2) + 80;
+    var card = "<img src='images/classic-cards/redVertFull.png'>";
+    var table = $('#table');
+    var dealerHand = new Array();
+    var playerHand = new Array();
+
+    for(var i = 0; i < numberOfCards; i++)
+    {
+        dealerHand.push(deck[0]);
+        deck.splice(0, 1);
+        playerHand.push(deck[0]);
+        deck.splice(0, 1);
+    }
+
+    var newLeft = 180;
+    for(var i = 0; i < numberOfCards; i++)
+    {
+        $('#table').append(card);
+        var cardObj = table.children().get(i);
+        $(cardObj).addClass('card');
+        $(cardObj).addClass('in-d-hand');
+        var dealerCardFace = dealerHand[i].face;
+        $(cardObj).attr('src', dealerCardFace);
+        $(cardObj).attr('data-card', JSON.stringify(dealerHand[i]));
+        dealerCardValue = dealerHand[i].value;
+        $(cardObj).css('left', newLeft);
+        $(cardObj).click(cardAlert);
+        newLeft += 12;
+    }
+    newLeft = 180;
+    var counter = 0;
+    for(var j = numberOfCards; j < numberOfCards * 2; j++)
+    {
+        $('#table').append(card);
+        var cardObj = table.children().get(j);
+        $(cardObj).addClass('card');
+        $(cardObj).addClass('in-p-hand');
+        var playerCardFace = playerHand[counter].face;
+        $(cardObj).attr('src', playerCardFace);
+        $(cardObj).attr('data-card', JSON.stringify(playerHand[counter]));
+        playerCardValue = playerHand[counter].value;
+        $(cardObj).css('left', newLeft);
+        $(cardObj).click(cardAlert);
+        counter++;
+        newLeft += 12;
+    }
+});
+    $('#hit').on('click', function hit()
         {
-        var card = "<img src='images/classic-cards/redVertFull.png'>";
-        var table = $('#table');
-        var dealerHand = new Array();
-        var playerHand = new Array();
-    
-        for(var i = 0; i < 2; i++)
-            {
-            dealerHand.push(deck[0]);
-            deck.splice(0, 1);
-            playerHand.push(deck[0]);
-            deck.splice(0, 1);
-            }
-    
-        var newLeft = 140;
-        for(var i = 0; i < 2; i++)
-            {
-            $('#table').append(card);
-            var cardObj = table.children().get(i); 
-            $(cardObj).addClass('card');
-            $(cardObj).addClass('in-d-hand');
-            var dealerCardFace = dealerHand[i].face;
-            $(cardObj).attr('src', dealerCardFace);
-            $(cardObj).attr('data-card', JSON.stringify(dealerHand[i]));
-            dealerCardValue = dealerHand[i].value;
-            $(cardObj).css('left', newLeft);
-            $(cardObj).click(cardAlert);
-            newLeft += 12;
-            }
-        newLeft = 140;
-        var counter = 0;
-        for(var j = 2; j < 4; j++)
-            {
-            $('#table').append(card);
-            var cardObj = table.children().get(j); 
-            $(cardObj).addClass('card');
-            $(cardObj).addClass('in-p-hand');
-            var playerCardFace = playerHand[counter].face;
-            $(cardObj).attr('src', playerCardFace);
-            $(cardObj).attr('data-card', JSON.stringify(playerHand[counter]));
-            playerCardValue = playerHand[counter].value;
-            $(cardObj).css('left', newLeft);
-            $(cardObj).click(cardAlert);
-            counter++;
-            newLeft += 12;
-            }
-        });
-
-$('#hit').on('click', function hit()
-    {
-    alert("Hit me, Baby!");
+        alert("Hit me, Baby!");
     });
 
-$('#stand').on('click', function stand()
-    {
-    alert("I'm good!");
+    $('#stand').on('click', function stand()
+        {
+        alert("I'm good!");
     });
 
-function shuffle(array) 
-	{
-  	var currentIndex = array.length, temporaryValue, randomIndex ;
+function shuffle(array)
+{
+    var currentIndex = array.length, temporaryValue, randomIndex ;
 
-  	while (0 !== currentIndex) 
-		{
-    	randomIndex = Math.floor(Math.random() * currentIndex);
-    	currentIndex -= 1;
-    	temporaryValue = array[currentIndex];
-    	array[currentIndex] = array[randomIndex];
-    	array[randomIndex] = temporaryValue;
-  		}
-  	return array;
-	}
-
+    while (0 !== currentIndex)
+    {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+};
