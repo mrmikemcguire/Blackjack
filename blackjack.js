03bde950a1f0e83687642611be04c820752dff4f
mcguire <mcguire@mullenhigh.com>
2015-10-23 14:06:31 -0600

    Stripped down one-card dealing

--- blackjack.js
+++ blackjack.js
@@ -1,86 +1,41 @@
 shuffle(deck);
 $('#sound')[0].play();
-var z = 1;
-
-
-function cardAlert (clickEvent)
-{
-    var $card, data;
-    $card = $(clickEvent.target);
-    data = $card.data('card');
-//  	alert(data.rank + " of " + data.suit);
-    $card.addClass('played');
-    $card.removeClass('in-d-hand');
-    $card.removeClass('in-p-hand');
-    $card.css('left', discardLocation);
-    $card.css('z-index', z);
-    z ++;
-}
+var dealerHand = new Array();
+var playerHand = new Array();
 
 $('#deal').on('click',function()
 {
-    numberOfCards = 2
-
-    var card = "<img src='images/classic-cards/redVertFull.png'>";
-    var table = $('#table');
-    var dealerHand = new Array();
-    var playerHand = new Array();
-
     for(var i = 0; i < 2; i++)
-    {
+        {
         dealerHand.push(deck[0]);
         deck.splice(0, 1);
         playerHand.push(deck[0]);
         deck.splice(0, 1);
-    }
+        }
 
-    var newLeft = 120;
-    for(var i = 0; i < 2; i++)
-    {
-        $('#table').append(card);
-        var cardObj = table.children().get(i);
-        //var cardObj = card;
+    var card = "<img src='images/classic-cards/redVertFull.png'>";
+    var $dealerCards = $('#dealerCards');
+    $dealerCards.append(card);
+    var cardObj = $dealerCards.children().get(0);
+    var dealerCardFace = dealerHand[0].face;
+    $(cardObj).attr('src', dealerCardFace);
+    $(cardObj).attr('data-card', JSON.stringify(dealerHand[0]));
+    dealerCardValue = dealerHand[0].value;
 
-        $(cardObj).addClass('card');
-        $(cardObj).addClass('in-d-hand');
-        var dealerCardFace = dealerHand[i].face;
-        $(cardObj).attr('src', dealerCardFace);
-        $(cardObj).attr('data-card', JSON.stringify(dealerHand[i]));
-        dealerCardValue = dealerHand[i].value;
-        $(cardObj).css('left', newLeft);
-        $(cardObj).click(cardAlert);
-        newLeft += 12;
-    }
-    newLeft = 120;
-    var counter = 0;
-    for(var j = 2; j < 4; j++)
-    {
-        $('#table').append(card);
-        var cardObj = table.children().get(j);
-        $(cardObj).addClass('card');
-        $(cardObj).addClass('in-p-hand');
-        var playerCardFace = playerHand[counter].face;
-        $(cardObj).attr('src', playerCardFace);
-        $(cardObj).attr('data-card', JSON.stringify(playerHand[counter]));
-        playerCardValue = playerHand[counter].value;
-        $(cardObj).css('left', newLeft);
-        $(cardObj).click(cardAlert);
-        counter++;
-        newLeft += 12;
-    }
 });
-    $('#hit').on('click', function hit()
-        {
-        alert("Hit me, Baby!");
+
+$('#hit').on('click', function hit()
+    {
+    alert("Hit me, Baby!");
     });
 
-    $('#stand').on('click', function stand()
-        {
-        alert("I'm good!");
+$('#stand').on('click', function stand()
+    {
+    alert("I'm good!");
     });
 
 function shuffle(array)
-{
+    {
     var currentIndex = array.length, temporaryValue, randomIndex ;
 
     while (0 !== currentIndex)
@@ -92,4 +47,4 @@ function shuffle(array)
         array[randomIndex] = temporaryValue;
     }
     return array;
-};
+    };
